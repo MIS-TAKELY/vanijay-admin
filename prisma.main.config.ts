e296ef8fbf0de5440556a585a4e9541d63cb405c
MISTAKELY <thereisamailforme@gmail.com>
2026-03-09 18:20:19 +0545

    fix: separate shared DB migrations from admin migrations

--- prisma.main.config.ts
+++ prisma.main.config.ts
@@ -2,7 +2,7 @@
 export default {
     schema: 'prisma/schema.main.prisma',
     migrations: {
-        path: 'prisma/migrations',
+        path: 'prisma/migrations-main',
     },
     datasource: {
         url: process.env.DATABASE_URL,
